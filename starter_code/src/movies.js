08d2f1d900dfe63c9e68901cdf74621b9b98f05f
Tarek koushaji <tarek.koushaji@gmail.com>
2020-01-17 08:58:21 +0100

    almost finished XD

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -78,8 +78,12 @@ function ratesAverage(arr) {
   }
 
   const rate = arr.reduce( (sum, movie) => {
-    if (movie.rate<=0) {
-      movie.rate = 2.00;
+    if (movie.rate>0) {
+      return sum + movie.rate;
+    } else if (!movie.rate) {
+      return sum;
+    } else if (movie.rate = 0) {
+      return sum + 2;
     }
     return sum + movie.rate;
   },0);
@@ -88,9 +92,137 @@ function ratesAverage(arr) {
   return Number((rate/arr.length).toFixed(2));
 }
 
-
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let newArr = arr.filter(arr => arr.genre.includes("Drama"));
+  if (newArr.length === 0) {
+    return 0;
+  }
+  const rate = newArr.reduce( (sum, movie) => {
+    if (movie.rate>0) {
+      return sum + movie.rate;
+    } else if (!movie.rate) {
+      return sum;
+    } else if (movie.rate = 0) {
+      return sum + 2;
+    }
+    return sum + movie.rate;
+  },0);
+
+  return Number((rate/newArr.length).toFixed(2));
+
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  if (arr.duration===undefined){
+    return 0;
+  }
+
+// method 0 :D
+  /*arr=arr.forEach(function (element){
+    if(!element.duration.includes("min")){
+      element.duration=parseInt(element.duration)*60;
+    }
+    else if(element.duration.includes("min")){
+      if(element.duration.includes(" ")){
+        let time=[];
+        time.push(element.duration.split(" "));
+         time[0]=parseInt(time[0])*60;
+         time[1]=parseInt(time[1]);
+         element.duration=time[0]+time[1];
+      }
+      else {
+        element.duration=parseInt(element.duration);
+      }
+    }
+    return arr;
+  })
+  */
+
+  //method 1 XDDD
+ /* arr=arr.forEach(function (element){
+    if(!element.duration.includes(" ")){
+      if(element.duration<15){
+        element.duration=parseInt(element.duration)*60;
+        return arr;
+     }else{
+      element.duration=parseInt(element.duration);
+      return arr;
+      }
+      }
+       else if(element.duration.includes(" ")){
+         let time=[];
+        time.push(element.duration.split(" "));
+         time[0]=parseInt(time[0])*60;
+         time[1]=parseInt(time[1]);
+         element.duration=time[0]+time[1];
+         return arr;
+        }
+      
+
+  })
+*/
+
+
+//method 2 XD 
+  //let newArr=[];
+  //for(let i=0;i<=arr.length;i++){
+  //  if(!arr[i].duration.includes(" ")){
+  //    if(arr[i].duration<15){
+  //     arr[i].duration=parseInt(arr[i].duration)*60;
+  //    }
+  //    else {
+  //      arr[i].duration=parseInt(arr[i].duration);
+  //    }
+  //  }
+  //  else if(arr[i].duration.includes(" ")){
+  //    let time=[];
+  //    time.push(arr[i].split(" "));
+  //   time[0]=parseInt(time[0])*60;
+  //    time[1]=parseInt(time[1]);
+  //    arr[i].duration=time[1]+time[0];
+  //  }
+  //  newArr.push(arr[i]);
+  //}
+
+
+
+//method 3 XD 
+
+
+  arr = arr.map(function(movie){
+    let filmTime=movie;
+    if (!(filmTime.duration).includes(" ")) {
+    if(parseInt(filmTime.duration)<=25){
+        filmTime.duration=parseInt(filmTime.duration);
+      }
+      else if (parseInt(filmTime.duration)>25){
+      filmTime.duration = parseInt(filmTime.duration)*60;
+      }
+    } else if ((filmTime.duration).includes(" ")){
+     let time = [];
+     time.push((filmTime.duration).split(" "));
+  
+      time[0]=parseInt(time[0])*60;
+      time[1]=parseInt(time[1]);
+      filmTime.duration=time[0]+time[1];
+    }
+    return filmTime;
+  })
+  
+}
+
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+function bestYearAvg(arr){
+  
+}
